6d60ea6c3939ba51dc3b3414802732a653698d9c
AssanSav <assane.savadogo81@gmail.com>
2020-04-30 00:13:45 -0400

    Done.

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeAuthor } from "../actions";
 
-class Authors extends Component {
+export class Authors extends Component {
+  constructor() {
+    super()
+    this.handleRemove = this.handleRemove.bind(this)
+  }
+
+  // handleRemove = (event) => {
+  //   debugger
+  //   event.preventDefault()
+  //   let author = this.props.authors.find(a => a.id === event.target.getAttribute("dataset"))
+  //   return this.props.removeAuthor(author)
+  // }
+
+  handleRemove(event) {
+    event.preventDefault()
+    let author = this.props.authors.find(a => a.id === event.target.getAttribute("dataset"))
+    return this.props.removeAuthor(author)
+  }
 
   render() {
-
-    let authors = this.props.authors.map(author => <li key={author.id}>{author.authorName}</li>);
+    let authors = this.props.authors.map(author => <li key={author.id}>{author.authorName}<button dataset={author.id} onClick={this.handleRemove}>X</button></li>);
 
     return (
       <div>
@@ -21,4 +38,5 @@ const mapStateToProps = state => {
   return { authors: state.authors }
 }
 
-export default connect(mapStateToProps)(Authors);
+
+export default connect(mapStateToProps, {removeAuthor})(Authors);
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeBook} from "../actions"
 
 class Books extends Component {
+  constructor() {
+    super()
+    this.handleRemove = this.handleRemove.bind(this)
+  }
+
+  handleRemove(event) {
+    let book = this.props.books.find(b => b.id === event.target.getAttribute("dataset"))
+    return this.props.removeBook(book)
+  }
 
   render() {
-
-    let books = this.props.books.map(book => <li key={book.id}>{book.title} by {book.authorName}</li>);
-
+    let books = this.props.books.map(book => <li key={book.id}>{book.title} by {book.authorName}<button dataset={book.id} onClick={this.handleRemove}>X</button></li>);
     return (
       <div>
         <ul>
@@ -21,4 +29,4 @@ const mapStateToProps = state => {
   return { books: state.books }
 }
 
-export default connect(mapStateToProps)(Books);
+export default connect(mapStateToProps, { removeBook})(Books);
